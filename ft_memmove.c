635b1a22bd73dfe2319c2b474f4cbe91d7afdbd8
Ilias Kaddouri <ilkaddou@paul-f4Br5s5.clusters.42paris.fr>
2024-11-09 17:57:03 +0100

    strtrim

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,45 +6,35 @@
 /*   By: ilkaddou <ilkaddou@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 22:50:14 by ilkaddou          #+#    #+#             */
-/*   Updated: 2024/11/08 16:50:52 by ilkaddou         ###   ########.fr       */
+/*   Updated: 2024/11/09 17:25:19 by ilkaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*dest;
-	char	*source;
 	size_t	i;
 
-	dest = (char *)dst;
-	source = (const char *)src;
+	if (dest == src || n == 0)
+		return (dest);
 	i = 0;
-	while (i < len &&)
+	if (dest < src)
 	{
+		while (i < n)
+		{
+			((char *)dest)[i] = ((const char *)src)[i];
+			i++;
+		}
 	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			((char *) dest)[i - 1] = ((const char *)src)[i - 1];
+			i--;
+		}
+	}
+	return ((char *)dest);
 }
-#include <stdio.h>
-#include <string.h>
-
-int	main(void)
-{
-	// test ft_memcpy
-	char src[] = "Hello, world!";
-	char dst[20];
-	memcpy(dst +5, src, strlen(src) + 1);
-	printf("%s\n", dst);
-
-	// test vrai memmove
-	char src2[] = "sfgager";
-	char dst2[20];
-	memmove(dst2 + 5, src2, strlen(src2) + 1);
-	printf("%s\n", dst2);
-
-	return (0);
-	char csrc[100] = "Geeksfor"; 
-memmove(csrc+5, csrc, strlen(csrc)+1); 
-printf("%s", csrc); 
-return 0; 
-}*/

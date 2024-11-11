5a65b9918f20112a0d12c5320e80c0cc73082c4d
Ilias Kaddouri <ilkaddou@bess-f3r4s11.clusters.42paris.fr>
2024-11-11 15:27:17 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,27 @@
 /*   By: ilkaddou <ilkaddou@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 16:46:04 by ilkaddou          #+#    #+#             */
-/*   Updated: 2024/11/10 16:48:25 by ilkaddou         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:18:00 by ilkaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	num_words (char const s, char c)
+{
+	int	i;
+	int	num;
+	
+	num = 1;
+	i = 0;
+	while (s[i])
+	{
+		if(s[i] = c)
+			num++;
+		i ++;
+	}
+	return (num);
+}
+
 char **ft_split(char const *s, char c)
 {
-	
+	int	num_words (char const s, char c)
 }
